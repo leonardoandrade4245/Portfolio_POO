a96a90ec806b69a6fff062033e4e3a9438b689dc
Leonardo Andrade <leonardo.andrade4245@gmail.com>
2025-07-14 22:59:26 -0300

    Atualizacao

--- Portfolio_POO/src/main/java/br/com/fecaf/Database/estoque.sql
+++ Portfolio_POO/src/main/java/br/com/fecaf/Database/estoque.sql
@@ -25,12 +25,27 @@ insert into marca(nome) values
 ("Nissan"),
 ("Volkswagen");
 
-insert into carro(nome, ano, classe, preco, modelo, marca_id) values
-("Toro", 2022 ,"picape", 126.000, "Ultra", 2),
-("Sentra", 2025, "sedan", 115.000 , "Advance", 4),
-("T-Cross", 2025, "suv", 120.000, "Confortline", 5),
-("Onix", 2025, "hatch", 110.000, "Turbo", 3),
-("Hilux", 2025, "picape", 145.000, "SRX Plus AT", 1);
+insert into carro(nome, ano, classe, preco, modelo, marca_id, foto, cor) values
+("Toro", 2022 ,"picape", 126.000, "Ultra", 2, "/Portfolio_POO/src/main/resources/static/img/Foto_FiatToro.JPG", "Branco"),
+("Sentra", 2025, "sedan", 115.000 , "Advance", 4, "/Portfolio_POO/src/main/resources/static/img/Foto_NissanSentra.jpg", "Branco"),
+("T-Cross", 2025, "suv", 120.000, "Confortline", 5, "/Portfolio_POO/src/main/resources/static/img/Foto_TCross.jpg", "Prata"),
+("Onix", 2025, "hatch", 110.000, "Turbo", 3, "/Portfolio_POO/src/main/resources/static/img/Foto_Onix.jpg", "Prata"),
+("Hilux", 2025, "picape", 145.000, "SRX Plus AT", 1, "/Portfolio_POO/src/main/resources/static/img/Foto_Hilux.jpg", "Branco");
 
 select * from marca;
 select * from carro;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+TRUNCATE TABLE carro
+SET FOREIGN_KEY_CHECKS = 1
+
+ALTER TABLE carro
+ADD cor VARCHAR(45) NOT NULL,
+ADD disponibilidade ENUM('A venda', 'Vendido') NOT NULL DEFAULT 'A venda';
+
+
+ALTER TABLE carro MODIFY disponibilidade ENUM('A venda', 'A_venda', 'Vendido') NOT NULL DEFAULT 'A_venda';
+UPDATE carro SET disponibilidade = 'A_venda' WHERE disponibilidade = 'A venda';
+ALTER TABLE carro MODIFY disponibilidade ENUM('A_venda', 'Vendido') NOT NULL DEFAULT 'A_venda';
+
